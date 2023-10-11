36503d3a2a1032a5072106196e6509835264b9a3
tracy <709830862@qq.com>
2023-10-11 10:28:33 +0800

    初始化design-mode

---
+++ src/test/java/com/geek/tracy/designmode/DisignModeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geek.tracy.designmode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DisignModeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
